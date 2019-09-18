d0f07e6bf6304c012325337cdc4762b488b3e871
rb2001 <rbaird2001@yahoo.com>
2019-09-18 15:32:07 -0600

    added do-what-this-says & moment() time format for concerts

--- liri.js
+++ liri.js
@@ -1,12 +1,16 @@
 
 require("dotenv").config();
-let keys = require("./keys.js");
-const Axios = function(){this.axios = require("axios")};
+const keys = require("./keys.js");
+const Axios = function () { this.axios = require("axios") };
 const Spotify = require("node-spotify-api");
+const fs = require("fs");
+const moment = require("moment");
 
 var liriType = process.argv[2];
 var liriParam = process.argv.slice(3).join("+");
- 
+
+
+
 //Add search method to Axios constructor 
 Axios.prototype.search = function (url) {
     this.url = url;
@@ -50,7 +54,7 @@ Axios.prototype.concertResults = function (promise) {
         function (str) {
             let list = ""
             for (i = 0; i < str.length; i++) {
-                list += `${str[i].venue.name}  --  ${str[i].venue.city}  ${str[i].venue.region}, ${str[i].venue.country}\n`;
+                list += `${moment(str[i].datetime).format("MM/DD/YYYY")} -- ${str[i].venue.name}  --  ${str[i].venue.city}  ${str[i].venue.region}, ${str[i].venue.country}\n`;
             }
             console.log(list);
             return true;
@@ -71,66 +75,70 @@ Axios.prototype.movieResults = function (promise) {
            Language: ${str.Language}\n
            Plot: ${str.Plot}
            `
-        );
-    });
+            );
+        });
 }
 
 var spotify = new Spotify({
     id: keys.spotify.id,
     secret: keys.spotify.secret
-  });
+});
 
-  spotifySearch = function(str){
+spotifySearch = function (str) {
     spotify
-      .search({ type: 'track', query: str, limit: 1})
-      .then(function(response) {
-        let ret = response.tracks.items[0];
-        //console.log(ret)
-        let list = 
-        `
+        .search({ type: 'track', query: str, limit: 1 })
+        .then(function (response) {
+            let ret = response.tracks.items[0];
+            //console.log(ret)
+            let list =
+                `
         Song: ${ret.name} 
            Artist(s): ${ret.artists[0].name}
            Album: ${ret.album.name}
            Preview: ${ret.preview_url}
         `
-        console.log(list);
-      })
-      .catch(function(err) {
-        console.log(err);
-      });
-    }
-//add search method to Spotify constructor
-//  Spotify.prototype.search = function (songTitle) {
-//     this.songTitle = songTitle 
-//     //run spotify search
-//     console.log(this.id)
-//     return spotify
-//         .search({ 
-//             type: 'track', 
-//             query:  this.songTitlesongTitle, 
-//             limit: 1
-//           })
-//         .then(function(response) {
-//           console.log(response.tracks.items[0]);
-//         })
-//         .catch(function(err) {
-//           console.log(err);
-//         })
-//  }
-
- //Add concert processing method to Axios constructor
+            console.log(list);
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+}
+doRandomTxt = function () {
+    fs.readFile("random.txt", "utf-8", function (err, data) {
+        if (err) {
+            console.log(`error: ${err}`);
+        }
+        let commandArray = data.replace(/"/g, "").split(",");
+            if(commandArray[0]==="do-what-it-says"){
+                console.log("infinite recursion")
+                return false
+            }
+            searchEvent(commandArray[0],commandArray[1]);
+    })
+}
 
 //create/call new functions based on the liri search type requested.
-if(liriType === "concert-this"){
-    let concertSearch = new Axios
-    concertSearch.url = `https://rest.bandsintown.com/artists/${liriParam}/events?app_id=codingbootcamp`;
-    return concertSearch.concertResults(concertSearch.search(concertSearch.url));
+searchEvent = function (liriType, liriParam) {
+    if (liriType === "concert-this") {
+        let concertSearch = new Axios
+        concertSearch.url = `https://rest.bandsintown.com/artists/${liriParam}/events?app_id=codingbootcamp`;
+        return concertSearch.concertResults(concertSearch.search(concertSearch.url));
+    }
+    if (liriType === "movie-this") {
+        let movieSearch = new Axios
+        movieSearch.url = `http://www.omdbapi.com/?apikey=d373dcb1&t=${liriParam}`;
+        return movieSearch.movieResults(movieSearch.search(movieSearch.url));
+    }
+    if (liriType === "spotify-this-song") {
+        console.log(spotifySearch(liriParam))
+        return true
+    }
+    if (liriType === "do-what-this-says") {
+        doRandomTxt();
+    }
+    else{
+        console.log(`unknown command`);
+        return false;
+    }
 }
-if(liriType === "movie-this"){
-    let movieSearch = new Axios
-    movieSearch.url = `http://www.omdbapi.com/?apikey=d373dcb1&t=${liriParam}`;
-    return movieSearch.movieResults(movieSearch.search(movieSearch.url));
-}
-if(liriType === "spotify-this-song"){
-    console.log(spotifySearch(liriParam))
-}
+searchEvent(liriType,liriParam)
--- scratch.js
+++ scratch.js
@@ -1,26 +1,70 @@
-// require("dotenv").config();
-// var keys = require("./keys.js");
-// var Spotify = require('node-spotify-api');
- 
-// var spotify = new Spotify({
-//   id: keys.spotify.id,
-//   secret: keys.spotify.secret
-// });
- 
-// spotify
-//   .search({ type: 'track', query: 'All the Small Things', limit: 1})
-//   .then(function(response) {
-//     console.log(response.tracks.items[0].album);
-//   })
-//   .catch(function(err) {
-//     console.log(err);
-//   });
-var axios = require("axios")
-axios
-        .get("http://www.omdbapi.com/?apikey=d373dcb1&t=shrek")
+const Axios = function () { this.axios = require("axios") };
+const moment = require("moment");
+//var liriType = process.argv[2];
+//var liriParam = process.argv.slice(2).join("+");
+
+let url = `https://rest.bandsintown.com/artists/weezer/events?app_id=codingbootcamp`;
+Axios.prototype.search = function (url) {
+    this.url = url;
+    console.log("preAxios");
+    console.log(this.url);
+    return this.axios
+        .get(this.url)
         .then(function (response) {
             // If the axios was successful...
             // Then log the body from the site!
-            console.log(response.data); //, response.data
+            timeFormatted = moment(response.data[0].datetime).format("MM/DD/YYYY");
+            console.log(timeFormatted);
             return response.data;
         })
+        .catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log("ERROR data", error.response.data, "ERROR status", error.response.status, "ERROR headers", error.response.headers);
+                return false
+            }
+            if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log("ERROR no response", error.request);
+                return false
+            }
+            if (error.config) {
+                // Something happened in setting up the request that triggered an Error
+                console.log("ERROR config", error.config);
+                return false
+            }
+            else {
+                console.log("ERROR request", error.message);
+                return false
+            }
+
+        });
+}
+
+let concert = new Axios;
+concert.search(url);
+
+/*
+[
+    {
+      offers: [ [Object] ],
+      venue: {
+        name: 'Jockey Club Del Peru Telepodromo San Roque',
+        country: 'Peru',
+        region: '',
+        city: 'Cercado De Lima',
+        latitude: '-12.1479304',
+        longitude: '-76.99187'
+      },
+      datetime: '2019-09-22T19:00:45',
+      on_sale_datetime: '',
+      description: '',
+      lineup: [ 'Weezer' ],
+      id: '101208102',
+      artist_id: '140',
+      url: 'https://www.bandsintown.com/e/101208102?app_id=codingbootcamp&came_from=267&utm_medium=api&utm_source=public_api&utm_campaign=event'
+    },
+]
+*/
